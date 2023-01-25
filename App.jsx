31dc04febc1a515fd0c64e21d14a01a5d1563c6b
Minji <minnienam0726@gmail.com>
2023-01-26 02:35:49 +0900

    init: 프로젝트 초기 설정

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView,Text,View } from 'react-native';
+
+function App() {
+
+  return (
+    <SafeAreaView>
+        <View>
+          <Text>
+            Calendar
+          </Text>
+        </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
