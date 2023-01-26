f54819af99fa52260563b885724865c3418560b3
Minji <minnienam0726@gmail.com>
2023-01-27 03:01:43 +0900

    feat: 하단 네비게이터 구현

---
+++ src/tabs/BottomTabs.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesomeIcons from "react-native-vector-icons/FontAwesome";
+
+import Home from "../screens/Home";
+import Calendar from "../screens/Calendar";
+import Library from "../screens/Library";
+import MyPage from "../screens/MyPage";
+
+function BottomTabs() {
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Calendar"
+          component={Calendar}
+          options={{
+            tabBarLabel: "Calendar",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcons name="calendar" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Library"
+          component={Library}
+          options={{
+            tabBarLabel: "Library",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcons name="picture-o" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="MyPage"
+          component={MyPage}
+          options={{
+            tabBarLabel: "MyPage",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcons
+                name="user-circle-o"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default BottomTabs;
