c097c44a402b009c09368f3a92db1a59b3197176
Minji <minnienam0726@gmail.com>
2023-01-28 04:00:30 +0900

    feat: 캘린더 선택한 날짜 표시 구현

--- src/screens/Calendar.jsx
+++ src/screens/Calendar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 function Calendar() {
   const [month, setMonth] = useState(() => new Date().getMonth());
+  const [pressedButton, setPressedButton] = useState("");
+
   const dayWeekArray = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const today = new Date();
   const monthString = new Date(today.getFullYear(), month, today.getDate()).toLocaleString("en-US", { month: "long" });
@@ -14,42 +16,54 @@ function Calendar() {
   const dayilyMatrix = new Array(6).fill([]).map(() => {
     return new Array(7).fill(0).map(() => {
       dateCount++;
-      return new Date(today.getFullYear(), month, dateCount).getDate();
+      return new Date(today.getFullYear(), month, dateCount);
     });
   });
 
   return (
     <View style={styles.container}>
       <View>
+        <Icon.Button
+          name="angle-left"
+          color="#35C0F5"
+          size={40}
+          backgroundColor="#00000000"
+          onPress={() => setMonth(month - 1)}
+        />
         <Text>
-          <Icon.Button
-            name="angle-left"
-            color="#2196F3"
-            backgroundColor="#00000000"
-            onPress={() => setMonth(month - 1)}
-          />
           {monthString} {year}
-          <Icon.Button
-            name="angle-right"
-            color="#2196F3"
-            backgroundColor="#00000000"
-            onPress={() => setMonth(month + 1)}
-          />
         </Text>
+        <Icon.Button
+          name="angle-right"
+          color="#35C0F5"
+          size={40}
+          backgroundColor="#00000000"
+          onPress={() => setMonth(month + 1)}
+        />
       </View>
       <View>
         <View>
-          <Text>{dayWeekArray}</Text>
-        </View>
-        <View>
-          {dayilyMatrix.map((weekList, weekIndex) => (
-            <Text key={weekIndex}>
-              {weekList.map((day, dayIndex) => (
-                <Text key={dayIndex}>{day}</Text>
-              ))}
+          {dayWeekArray.map((dayWeek, dayWeekIndex) => (
+            <Text key={dayWeekIndex}>
+              {dayWeek}
             </Text>
           ))}
         </View>
+        {dayilyMatrix.map((weekList, weekIndex) => (
+          <Text key={weekIndex}>
+            {weekList.map((day, dayIndex) => (
+              <TouchableOpacity
+                key={dayIndex}
+                style={pressedButton.toString() === weekList[dayIndex].toString() ? styles.radiusEffect : styles.normal}
+                onPress={() => setPressedButton(day)}
+              >
+                <Text>
+                  {day.getDate()}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </Text>
+        ))}
       </View>
     </View>
   );
